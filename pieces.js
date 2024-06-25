8037730f0bda521025d10690c4885ec3214020f3
Mouzone <sunnyliu010@gmail.com>
2024-06-24 20:12:06 -0400

    create classes for pieces that inherit from a piece class

---
+++ pieces.js
@@ -0,0 +1,30 @@
+class Piece {
+    constructor(x_start_pos, y_start_pos, color) {
+        this.x_pos = x_start_pos
+        this.y_pos = y_start_pos
+        this.color = color
+    }
+}
+
+class Pawn extends Piece {
+}
+
+class Rook extends Piece {
+
+}
+
+class Knight extends Piece {
+
+}
+
+class Bishop extends Piece {
+
+}
+
+class King extends Piece {
+
+}
+
+class Queen extends Piece {
+
+}
