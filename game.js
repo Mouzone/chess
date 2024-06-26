e1555998c89718c515df979939439e7ee8f95cee
Mouzone <sunnyliu010@gmail.com>
2024-06-26 19:52:17 -0400

    Refactor movement logic so pawns and other types share same block of code

--- game.js
+++ game.js
@@ -5,91 +5,64 @@
 //      then mark that piece as dead (easier than to find specific one to pop
 
 import {Black, White} from "./player.js"
-import {valid_move, get_color_of_tile, translate_position, updateBoard} from "./helper"
+import {valid_move, get_color_of_tile, translate_position, updateBoard, fillBoard} from "./helper"
 
 function process_move(move){
     const types = {"K":"kings", "Q":"queens", "R":"rooks", "N":"knights", "B":"bishops"}
-    if (!(move.at(0) in Object.keys(types))) {
-        // must be a pawn
-        // if typo will check move and run
-        if (!("x" in move)) {
-            let specification = null
-            if (move.length === 3) {
-                specification = move.at(1)
+    let type_of_piece = "pawns"
+    if (move.at(0) in Object.keys(types)){
+        type_of_piece = types[move.at(0)]
+    }
+    
+    if (!("x" in move)) {
+        let counter = 0
+        let specification = null
+        if (move.length > 3) {
+            specification = move.at(1)
+        }
+        const next_pos= translate_position(move.slice(-2))
+        let piece_to_move = ""
+        players[curr_player_turn].pieces[type_of_piece].forEach(piece => {
+            if (!piece.alive) {
+                return
             }
+            if (specification) {
+                if (typeof specification === "number") {
+                    if (piece.y_pos !== specification) {
+                        return
+                    }
+                } else if (typeof specification === "string") {
+                    if (piece.x_pos !== specification) {
+                        // figure out logic to check between letter and x_pos which is int
+                        return
+                    }
+                }
+            }
+            if (piece.check_valid_move(next_pos["file"], next_pos["rank"])){
+                counter++
+                piece_to_move = piece
+            }
+        })
+        if (counter === 0) {
+            console.error("Illegal Move")
+        } else if (counter === 1) {
+            piece_to_move.move(next_pos["file"], next_pos["rank"])
+        } else {
+            console.error("Not Specific Enough")
+        }
+    } else {
+        // split it at the "x"
+        // if something to the left of x we find specific pawn, else iterate through all and check valid
+        let capture_info = move.split("x")
+        if (capture_info.length === 1) {
+            const next_pos = translate_position(capture_info[0])
             let counter = 0
-            const next_pos = translate_position(move.slice(-2))
-            // fix this
-            const piece_to_move = players[curr_player_turn].pieces["pawns"].forEach(pawn => {
+            const piece_to_move = players[curr_player_turn].pieces[type_of_piece].forEach(pawn => {
                 if (pawn.check_valid_move(next_pos["file"], next_pos["rank"])){
                     counter++
                     return pawn
                 }
             })
-            if (counter === 0) {
-                console.error("Illegal Move")
-            } else if (counter === 1) {
-                piece_to_move.move(next_pos["file"], next_pos["rank"])
-            } else {
-                console.error("Not Specific Enough")
-            }
-        } else {
-            // split it at the "x"
-            // if something to the left of x we find specific pawn, else iterate through all and check valid
-            let capture_info = move.split("x")
-            if (capture_info.length === 1) {
-                const next_pos = translate_position(capture_info[0])
-                let counter = 0
-                const piece_to_move = players[curr_player_turn].pieces["pawns"].forEach(pawn => {
-                    if (pawn.check_valid_move(next_pos["file"], next_pos["rank"])){
-                        counter++
-                        return pawn
-                    }
-                })
-                // create matrix representation of the board and when game starts fill it up with the pieces
-            }
-        }
-    } else {
-        if (!("x" in move)) {
-            let counter = 0
-            let specification = null
-            if (move.length > 3) {
-                specification = move.at(1)
-            }
-            const next_pos= translate_position(move.slice(-2))
-            const type_of_piece = types[`${move.at(0)}`]
-            let piece_to_move = ""
-            players[curr_player_turn].pieces[type_of_piece].forEach(piece => {
-                if (!piece.alive) {
-                    return
-                }
-
-                if (specification) {
-                    if (typeof specification === "number") {
-                        if (piece.y_pos !== specification) {
-                            return
-                        }
-                    } else if (typeof specification === "string") {
-                        if (piece.x_pos !== specification) {
-                            // figure out logic to check between letter and x_pos which is int
-                            return
-                        }
-                    }
-                }
-                if (piece.check_valid_move(next_pos["file"], next_pos["rank"])){
-                    counter++
-                    piece_to_move = piece
-                }
-            })
-            if (counter === 0) {
-                console.error("Illegal Move")
-            } else if (counter === 1) {
-                piece_to_move.move(next_pos["file"], next_pos["rank"])
-            } else {
-                console.error("Not Specific Enough")
-            }
-        } else {
-            // capture logic
         }
     }
     curr_player_turn = !curr_player_turn
@@ -100,7 +73,7 @@ const BOARD_LENGTH = 8
 const board = Array(BOARD_LENGTH).fill().map(() =>
                     Array(BOARD_LENGTH).fill(null));
 let curr_player_turn = 1
-updateBoard(players, board)
+fillBoard(players, board)
 // update this with the take logic
 // also will need to create checkmate logic
 let game_over = false
