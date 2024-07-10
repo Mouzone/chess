03c4b52ad012670a442d87cb14a71fe35450f3be
Mouzone <sunnyliu010@gmail.com>
2024-07-10 19:47:21 -0400

    Delete unnecarrily complicated files, to simplify project

--- game.js
+++ game.js
@@ -1,15 +1,3 @@
-// entirely text-based
-// when moving check if possible and if only few are possible
-// when capturing check if can capture
-//      then move piece there
-//      then mark that piece as dead (easier than to find specific one to pop
-
-import {Black, White} from "./player.js"
-import {valid_move, get_color_of_tile, updateBoard, fillBoards, createBoard} from "./helper.js"
-
-// todo: on drag remove from square
-// todo: check square if valid move -> if not return to original square, else move it and change turn
-// todo: highlight squares that accept the move
 let curr_player_turn = 1
 let game_over = false
 
